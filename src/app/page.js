44811fe0f152f3cf35b69f46c278f55720a8f9b4
sharkipelago <gchozick@gmail.com>
2023-05-12 17:54:01 -0400

    Added logo on homepage

--- src/app/page.js
+++ src/app/page.js
@@ -4,40 +4,22 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
       <div className={styles.center}>
         <Image
           className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
+          src="/sgd-logo.svg"
+          alt="SGD Logo"
+          width={360}
+          height={200}
           priority
         />
       </div>
+
+      <p className={styles.welcome}>
+        Welcome to the homepage of Student Game Developers! We are a student-run organization focused
+        on creating original games and expanding knowledge about game design as an art and industry.
+      </p>
 
       <div className={styles.grid}>
         <a
