0403d13369b0a0f87d161f891c9e8d46d63558f9
sharkipelago <gchozick@gmail.com>
2023-05-19 21:35:16 -0400

    Added first draft navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Navbar from '@/components/navbar'
 import GameCard from '@/components/game-card'
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Navbar />
       <div className={styles.landing}>
         <Image 
           className={styles.hero}
