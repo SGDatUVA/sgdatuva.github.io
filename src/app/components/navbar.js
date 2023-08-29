8bbcd3525f49bba6efeaccb0b5b3d2863ff5b191
sharkipelago <gchozick@gmail.com>
2023-08-29 17:49:27 -0400

    Added itch page to games link

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
       </Link>
       <div className="flex items-center justify-evenly basis-5/12 font-bold text-2xl">
         <Link className="hover:text-highlight" href="/" >About</Link>
-        <Link className="hover:text-highlight" href="/">Games</Link>
+        <Link className="hover:text-highlight" href="https://uvasgd.itch.io/">Games</Link>
         <Link className="hover:text-highlight" href="/">Calendar</Link>
         <Link className="hover:text-highlight" href="/">Wiki</Link>
         <Link className="hover:text-highlight w-10" href="/">
