2dc72bbd81d60bb57a0e896d610e06699cf45ba2
sharkipelago <gchozick@gmail.com>
2023-08-29 18:22:12 -0400

    Added pictures and links to game cards

--- src/app/components/game-card.js
+++ src/app/components/game-card.js
@@ -1,13 +1,16 @@
 import BgImage from "./bg-image"
 
-export default function GameCard({name, background, className}) {
+export default function GameCard({name, background, href, className}) {
   return(
-    <div className={`relative bg-secondary h-60 w-64 rounded-lg text-primary hover:text-highlight flex
-      border-4 border-current transition duration-150 ease-in-out` + className}>
-      <BgImage source={background} className="opacity-100"/>
-      <div className="h-1/3 w-full bg-background bg-opacity-20 self-end flex justify-center text-center items-center">
-        <h3 className="text-lg font-semibold">{name}</h3>
+    <a href={href}>
+      <div className={`relative h-60 w-64 rounded-lg text-primary hover:text-highlight flex
+        border-4 border-current transition duration-150 ease-in-out` + className}>
+          <BgImage source={background} className="opacity-100"/>
+          <div className="h-1/3 w-full bg-background bg-opacity-20 self-end flex justify-center text-center items-center">
+            <h3 className="text-lg font-semibold">{name}</h3>
+          </div>
       </div>
-    </div>
+    </a> 
+
   )
 }
