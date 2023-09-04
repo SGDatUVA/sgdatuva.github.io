85b068ec76db5647e6132992bbd31ca009d814c6
sharkipelago <gchozick@gmail.com>
2023-09-04 09:36:30 -0400

    Basic responsivness on FAQ section

--- src/app/page.js
+++ src/app/page.js
@@ -35,8 +35,8 @@ export default function Home() {
       {/* Last Semester Games Section */}
       <div className='w-full relative border-t-2 border-primary md:h-[50vh]'>
         <BgImage source="/stock-computer.jpg" className="object-center"/>
-        <div className='h-full w-full flex flex-col items-center my-4 md:flex-row xl:pl-16 md:my-0'>
-          <h2 className='text-3xl text-primary font-bold text-center mb-2 md:m-0 md:basis-1/4 lg:text-4xl'>Spring 2023<br/>Games</h2>
+        <div className='h-full w-full flex flex-col items-center justify-center my-4 gap-x-8 md:flex-row md:my-0'>
+          <h2 className='text-3xl text-primary font-bold text-center mb-2 md:m-0 md:basis-1/6 lg:text-4xl'>Spring 2023<br/>Games</h2>
           <div className='flex flex-col space-y-4 md:space-x-4 md:space-y-0 md:flex-row lg:space-x-8'>
               <GameCard name="Flammable Forest" href="https://uvasgd.itch.io/flammable-forest" background="/flammableforest-thumbnail.png"/>
               <GameCard name="Across the Sea of Dust and Rot" href="https://buzjr.itch.io/across-the-sea-of-dust-and-rot" background="/dustandrot-thumbnail.png"/>
@@ -46,21 +46,21 @@ export default function Home() {
       </div>
 
       {/* FAQ */}
-      <div className='h-[50vh] w-full flex flex-col relative border-t-2 border-primary'>
+      <div className='w-full relative border-t-2 flex justify-center border-primary xl:h-[50vh]'>
         <BgImage source="/game-jam.png" className="object-center opacity-60"/>
-        <div className='h-full w-full flex items-center justify-center space-x-8'>
+        <div className='h-full w-full grid grid-cols-1 items-center justify-items-center my-4 gap-y-4 md:grid-cols-2 
+        xl:grid-cols-4 xl:gap-x-4 xl:my-0'>
           <FAQCard question='Where and when does SGD meet?' answer='Our next event will be our September pitch night where the semester projects will be introduced. More details soon. '></FAQCard>
           <FAQCard question='Does SGD do anything besides make games?' answer='SGD is primarily a game development club, but we also have social events and host sessions with industry professionals. '></FAQCard>
           <FAQCard question='What kind of software does SGD use?' answer='SGD projects mainly use the Unity game engine and github for source control, but all forms of game making are welcome!'></FAQCard>
           <FAQCard question='Who can I contact for more info?' answer='For any further questions feel free to reach out to uvasgd@gmail.com or contact any officers in the discord server. '></FAQCard>
         </div>
-
+      </div>
 
 
         {/* Copyright */}
-        <div className='h-[100px] w-full bg-background self-end	border-t-2 border-primary flex items-center justify-center'>
+      <div className='h-[80px] w-full bg-background self-end	border-t-2 border-primary flex items-center justify-center'>
           <p>Copyright © Student Game Developers 2023</p>
-        </div>
       </div>
 
     </main>
